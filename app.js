90fb6f4edad32e5075b921f992b13228d47ddadc
vincentpro <vincent_pro@hotmail.fr>
2020-10-21 11:47:21 +0200

    ready to deploy

--- app.js
+++ app.js
@@ -56,12 +56,15 @@ app.use("/auth", authRouter);
 const streamerRouter = require("./routes/streamer");
 app.use("/", streamerRouter);
 
-
-
-
 const scheduleRouter = require("./routes/schedule");
 app.use("/", scheduleRouter);
 
+if (process.env.NODE_ENV === "production") {
+    app.use("*", (req, res, next) => {
+        // If no routes match, send them the React HTML.
+        res.sendFile(__dirname + "/public/index.html");
+    });
+}
 
 
 
