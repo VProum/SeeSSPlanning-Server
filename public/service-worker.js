0860d64da96babf5e2ac74f11f666ddef28f0e01
vincentpro <vincent_pro@hotmail.fr>
2020-10-21 18:30:27 +0200

    sync with front

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6826fc70590c94265ba7259db6085490.js"
+  "/precache-manifest.6b06bdde56592967a27217b2331f5792.js"
 );
 
 self.addEventListener('message', (event) => {
